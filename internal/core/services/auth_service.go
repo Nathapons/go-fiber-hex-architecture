9eef0d28b0e88df1515f786f79ec504fdc7acbe8
Nuthapon <nuthaponm79@gmail.com>
2025-06-08 22:58:57 +0700

    feat: generate jwt

--- internal/core/services/auth_service.go
+++ internal/core/services/auth_service.go
@@ -1,1 +1,34 @@
 package services
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Claims struct {
+	UserID string `json:"user_id"`
+	Role   string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+// สร้างฟังก์ชั่นสำหรับสร้าง JWT
+func GenerateJWT(userID uint, role string) (string, error) {
+	secret := os.Getenv("JWT_SECRET")
+
+	claims := &Claims{
+		UserID: fmt.Sprintf("%d", userID),
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			Issuer:    "fier-ecommerce-api",
+			NotBefore: jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	return token.SignedString([]byte(secret))
+}
